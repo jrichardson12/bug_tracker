5c0b9dac640f1c7b6d130ee06f127afea109a9f3
John Richardson <John.Richardson@panerabread.com>
2019-11-20 15:20:29 -0600

    added more components

---
+++ src/projects/project_list.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectList = () => (
+  <ProjectView />
+)
+
+export default ProjectList;
---
+++ src/projects/project_view.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProjectView from './project_view.js'
+
+const ProjectView = () => (
+  <div>Project View</div>
+)
+
+export default ProjectView;
